8c74550306d4676c40314a1d6ad90c362b8374d8
Asera <kpo9101@gmail.com>
2026-06-20 16:51:45 +0900

    [add] Build.cs 정의

---
+++ ModuleAndPlugin/Source/Test/Test.Build.cs
@@ -0,0 +1,21 @@
+﻿using UnrealBuildTool;
+
+public class Test : ModuleRules
+{
+	public Test(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		// TestActor.h가 AActor, UObject 계열 타입을 공개적으로 사용하므로 Public에 둡니다.
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[]
+		{
+		});
+	}
+}
